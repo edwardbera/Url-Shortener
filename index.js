be1d5f4f7e8eec0a80137b14c959ac8bea880e63
edwardbera <beraedward@gmail.com>
2023-11-19 20:43:25 +0200

    Added short url response

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ mongoose.connect(process.env.MONGODBURL, {useNewUrlParser: true, useUnifiedTopol
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+
 app.get("/", function (request, response) {
     response.render("index");
   });
 
 app.post('/shorten', async (req, res) =>{
- await UrlModel.create({fullUrl : req.body.url})
- res.send(200)
+   // console.log(req.body.body.url)
+ const ap = await UrlModel.create({fullUrl : req.body.body.url})
+   // console.log('localhost:'+listener.address().port+'/'+ap.shortUrl)
+ res.send( listener.address().address+'/'+ap.shortUrl)
 
 })
 
 app.get('/getUrls', async(req, res)=>{
     const urls = await UrlModel.find();
-   
     res.send(urls)
 })
 
@@ -32,9 +34,9 @@ app.get('/:shortUrl', async (req, res)=>{
     if (url == null) return res.sendStatus(404)
     url.clicks++
     url.save()
-    res.redirect(url.full)
+    res.redirect(url.fullUrl)
 })
 
-  var listener = app.listen(3000, function () {
+  var listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
   });
